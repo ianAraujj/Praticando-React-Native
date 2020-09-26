7a4337f6d6f10e539c7b6bdfcf40cd3fe402e404
ianAraujj <ianlucas2503@gmail.com>
2020-09-26 02:26:20 -0300

    Foi adicionada a funcionalidade de listagem de Usuário do Github

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,20 +1,84 @@
-import React from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, TextInput, FlatList, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { RectButton } from 'react-native-gesture-handler';
+import { Keyboard } from 'react-native';
 
 import { styles } from '../styles/styles';
+import api from '../services/api'
 
-function alerta(text) {
-    alert("Voce digitou: " + text);
-}
+export default class Main extends Component {
 
-const Main = () => {
-    return (
-        <View style={styles.container}>
-            <View>
-                <TextInput placeholder="Adicionar" style={{ height: Platform.OS == 'android' ? 40 : 20 }}/>
+    state = {
+        usuarios: [],
+        novoUsuario: "",
+    }
+
+    adicionarNovoUsuario = async () => {
+        const novoUsuario = this.state.novoUsuario;
+
+        try {
+
+            const response = await api.get(`/users/${novoUsuario}`);
+            const status = response.status;
+
+            const data = {
+                login: response.data.login,
+                name: response.data.name,
+                avatar: response.data.avatar_url,
+                bio: response.data.bio
+            }
+
+            var usuarios = this.state.usuarios;
+            usuarios.push(data);
+            this.setState({
+                usuarios: usuarios,
+                newUser: ""
+            });
+
+            alert(data.bio);
+
+        }
+        catch (e) {
+            alert("Usuário Não Encontrado");
+        }
+
+        Keyboard.dismiss();
+
+    }
+
+
+
+    render() {
+
+        const renderItemUser = ({ item }) => (
+            <View style={styles.card}>
+                <Image source={{uri: item.avatar}} style={styles.imagemAvatar}/>
+                <Text>Nome: {item.name}</Text>
+                <Text>Bio: {item.bio}</Text>
             </View>
-        </View>
-    );
-}
+        );
 
-export default Main;
+        return (
+            <View style={styles.container}>
+                <View style={styles.formulario}>
+                    <View style={styles.TextInput}>
+                        <TextInput placeholder="Adicionar usuário"
+                            style={{ height: Platform.OS == 'android' ? 40 : 20 }}
+                            autoCorrect={false} autoCapitalize="none"
+                            onChangeText={(text) => this.setState({ novoUsuario: text })}
+                            returnKeyType="send"
+                            onSubmitEditing={this.adicionarNovoUsuario} />
+                    </View>
+
+                    <RectButton style={styles.botao} onPress={() => this.adicionarNovoUsuario()}>
+                        <Icon name="add" size={30} color="#FDFEFE" />
+                    </RectButton>
+                </View>
+                <View style={styles.separador} />
+                <FlatList data={this.state.usuarios} keyExtractor={(item) => item.login}
+                    renderItem={renderItemUser} />
+            </View >
+        );
+    }
+}
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -12,6 +12,31 @@ export const styles = StyleSheet.create({
         marginVertical: 8,
         borderBottomColor: '#737373',
         borderBottomWidth: StyleSheet.hairlineWidth,
+    },
+    formulario:{
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    botao:{
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#E74C3C',
+        marginLeft: 5
+    },
+    TextInput:{
+        flex: 1,
+        backgroundColor: '#D7DBDD',
+        paddingLeft: 8,
+    },
+    card:{
+        paddingBottom: 4,
+        backgroundColor: '#E5E7E9'
+    },
+    imagemAvatar:{
+        backgroundColor: '#F4F6F6',
+        height: 64,
+        width: 64,
+        borderRadius: 32
     }
 });
 
